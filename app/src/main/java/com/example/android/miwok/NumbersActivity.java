6b62ec056a05a3ba3a68cd023043ff87a94b86b6
Mohammed Almekhlafy <53212235+Blankedmaker@users.noreply.github.com>
2019-08-02 15:25:17 +0300

    Change Numbers implementation from LinearLayout to ListView

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -27,14 +30,18 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
+        //ListView implementation, good for memory!
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
+    /*LinearLayout implementation (bad for memory on very long lists)
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.list);
         for(int i = 0; i < words.size();i++){
             TextView wordsView = new TextView(this);
             wordsView.setText(words.get(i));
             rootView.addView(wordsView);
-        }
+        }*/
 
 
     }
